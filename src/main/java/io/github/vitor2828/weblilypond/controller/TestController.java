fff0fc35306c3ddd4c85d161293b26593381c847
vitor2828 <ak.jvitor28@gmail.com>
2026-02-02 14:48:15 -0300

    playing with springboot

--- src/main/java/io/github/vitor2828/weblilypond/controller/TestController.java
+++ src/main/java/io/github/vitor2828/weblilypond/controller/TestController.java
@@ -2,6 +2,8 @@ package io.github.vitor2828.weblilypond.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class TestController {
@@ -9,5 +11,10 @@ public class TestController {
     @GetMapping("/")
     public String home() {
         return "Spring Boot lives...";
+    }
+
+    @PostMapping("/compile")
+    public String compile(@RequestBody String body) {
+        return "ok";
     }
 }
